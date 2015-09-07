9f8df771a60228294c4692c5912e10d231c615e8
Toshifumi Sakaguchi <sischkg@gmail.com>
2015-09-07 21:48:48 +0900

    DNSサーバ要クラスを作成

--- src/dns_server.cpp
+++ src/dns_server.cpp
@@ -1,98 +1,101 @@
-#include "udpv4server.hpp"
-#include "dns.hpp"
-#include <string>
+#include "dns_server.hpp"
 #include <stdexcept>
 #include <iostream>
+#include <time.h>
+#include <boost/cstdint.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/numeric/conversion/cast.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/thread.hpp>
 
-const char *MY_ADDRESS      = "192.168.0.102";
-const char *MY_DOMAIN       = "example.com";
-const char *BIND_ADDRESS    = "192.168.0.1";
-const int   TTL             = 600;
-const int   NS_RECORD_COUNT = 2;
-const int   SUBDOMAIN_SIZE  = 30;
-const int   BUF_SIZE        = 256 * 256;
-
-
-dns::ResponsePacketInfo generate_response( uint16_t id, const dns::QuestionSectionEntry question_section )
+namespace dns
 {
-    dns::ResponsePacketInfo response;
-    response.id = id;
-    response.recursion_available = false;
-    response.truncation          = false;
-    response.authentic_data      = false;
-    response.checking_disabled   = false;
-    response.response_code       = dns::NXDOMAIN;
 
-    dns::QuestionSectionEntry question;
-    question.q_domainname = question_section.q_domainname;
-    question.q_class      = dns::CLASS_IN;
-    question.q_type       = dns::TYPE_A;
-    response.question.push_back( question );
-
-    return response;
-}
-
-struct BadChecksumCalculator : public udpv4::ChecksumCalculatable
-{
-    uint16_t operator()( const udpv4::PacketInfo & ) const
+    void DNSServer::startUDPServer()
     {
-	return 0;
-    }
-};
+	try {
+	    udpv4::ServerParameters params;
+	    params.bind_address = bind_address;
+	    params.bind_port    = bind_port;
+	    udpv4::Server dns_receiver( params );
 
+	    while( true ) {
+		try {
+		    udpv4::PacketInfo recv_data     = dns_receiver.receivePacket();
+		    QueryPacketInfo   query         = parse_dns_query_packet( recv_data.begin(), recv_data.end() );
+		    ResponseInfo      response_info = generateResponse( query );
 
-int main( int arc, char **argv )
-{
-    try {
-	udpv4::ServerParameters param;
-	param.bind_address = BIND_ADDRESS;
-	param.bind_port    = 53;
-
-	udpv4::Receiver dns_receiver( BIND_ADDRESS, 53 );
-	udpv4::Sender   dns_sender( udpv4::Sender::ChecksumPtr( new udpv4::StandardChecksumCalculator ) );
-	//	udpv4::Sender   dns_sender( udpv4::Sender::ChecksumPtr( new BadChecksumCalculator ) );
-
-	while( true ) {
-	    udpv4::PacketInfo recv_data, send_data;
-	    dns::QueryPacketInfo query;
-	    dns::ResponsePacketInfo response;
-	    std::vector<uint8_t> response_packet;
-
-	    try {
-		recv_data = dns_receiver.receivePacket();
-		query = dns::parse_dns_query_packet( recv_data.begin(), recv_data.end() );
-		std::cerr << "received" << std::endl;
-	    }
-	    catch( std::bad_cast &e ) {
-		std::cerr << "parse query failed," << std::endl;
-		std::exit( 1 );
-	    }
-
-	    try {
-		response = generate_response( query.id, query.question[0] );
-		response_packet = dns::generate_dns_response_packet( response );
-	    }
-	    catch( std::bad_cast &e ) {
-		std::cerr << "make response failed," << std::endl;
-		std::exit(1 );
-	    }
-
-	    try {
-		send_data.destination_address = recv_data.source_address;
-		send_data.destination_port    = recv_data.source_port;
-		send_data.source_address      = MY_ADDRESS;
-		send_data.source_port         = 53;
-		dns_sender.sendPacket( send_data );
-	    }
-	    catch( std::bad_cast &e ) {
-		std::cerr << "send response failed," << std::endl;
-		std::exit(1 );
+		    PacketData response_packet = generate_dns_packet( response_info.header,
+								      response_info.question_section, 
+								      response_info.answer_section,
+								      response_info.authority_section,
+								      response_info.additional_infomation_section );
+		
+		    udpv4::ClientParameters client;
+		    client.destination_address = recv_data.source_address;
+		    client.destination_port    = recv_data.source_port;
+		    dns_receiver.sendPacket( client, response_packet );
+		}
+		catch( std::runtime_error &e ) {
+		    std::cerr << "send response failed," << std::endl;
+		    std::exit(1 );
+		}
 	    }
 	}
-    }
-    catch ( std::runtime_error &e ) {
-	std::cerr << "caught " << e.what() << std::endl;
+	catch ( std::runtime_error &e ) {
+	    std::cerr << "caught " << e.what() << std::endl;
+	}
     }
 
-    return 0;
+
+    void DNSServer::startTCPServer()
+    {
+	try {
+	    tcpv4::ServerParameters params;
+	    params.bind_address = bind_address;
+	    params.bind_port    = bind_port;
+
+	    tcpv4::Server dns_receiver( params );
+
+	    while( true ) {
+
+		try {
+		    tcpv4::ConnectionPtr connection = dns_receiver.acceptConnection();
+		    PacketData size_data = connection->receive( 2 );
+		    uint16_t size = ntohs( *( reinterpret_cast<const uint16_t *>( &size_data[0] ) ) );
+
+		    PacketData      recv_data     = connection->receive( size );
+		    QueryPacketInfo query         = parse_dns_query_packet( &recv_data[0], &recv_data[0] + recv_data.size() );
+		    ResponseInfo    response_info = generateResponse( query );
+
+		    PacketData response_packet = generate_dns_packet( response_info.header,
+								      response_info.question_section, 
+								      response_info.answer_section,
+								      response_info.authority_section,
+								      response_info.additional_infomation_section );
+		
+		    uint16_t send_size = htons( response_packet.size() );
+		    connection->send( reinterpret_cast<const uint8_t *>( &send_size ), sizeof(send_size) );
+		    connection->send( response_packet );
+		}
+		catch( std::runtime_error &e ) {
+		    std::cerr << "send response failed(" << e.what() << ")." << std::endl;
+		    std::exit(1 );
+		}
+	    }
+	}
+	catch ( std::runtime_error &e ) {
+	    std::cerr << "caught " << e.what() << std::endl;
+	}
+    }
+
+
+    void DNSServer::start()
+    {
+	boost::thread udp_server_thread( &DNSServer::startUDPServer, this );
+	boost::thread tcp_server_thread( &DNSServer::startTCPServer, this );
+
+	udp_server_thread.join();
+	tcp_server_thread.join();
+    }
 }
---
+++ src/dns_server.hpp
@@ -0,0 +1,43 @@
+#ifndef DNS_SERVER_HPP
+#define DNS_SERVER_HPP
+
+#include "udpv4server.hpp"
+#include "tcpv4server.hpp"
+#include "dns.hpp"
+#include <string>
+
+namespace dns
+{
+    struct ResponseInfo {
+	PacketHeaderField                 header;    
+	std::vector<QuestionSectionEntry> question_section;
+	std::vector<ResponseSectionEntry> answer_section;
+	std::vector<ResponseSectionEntry> authority_section;
+	std::vector<ResponseSectionEntry> additional_infomation_section;
+    };
+
+    class DNSServer
+    {
+    private:
+	std::string bind_address;
+	uint16_t    bind_port;
+
+	void startUDPServer();
+	void startTCPServer();
+
+    public:
+	DNSServer( const std::string &address = "0.0.0.0",
+		   uint16_t           port = 53 )
+	    : bind_address( address ), bind_port( port )
+	{}
+
+	~DNSServer(){}
+
+	virtual ResponseInfo generateResponse( const QueryPacketInfo &query ) = 0;
+
+	void start();
+    };
+    
+}
+
+#endif
