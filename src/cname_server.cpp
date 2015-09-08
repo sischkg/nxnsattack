67fc99c7b7c4d83ed2206a8595c5c9de90e7061c
Toshifumi Sakaguchi <sischkg@gmail.com>
2015-09-08 22:18:21 +0900

    cnameを応答するサーバを追加

--- src/cname_server.cpp
+++ src/cname_server.cpp
@@ -2,15 +2,16 @@
 #include <boost/program_options.hpp>
 
 
-const int   TTL          = 600;
+const int   TTL          = 5;
 const char *RESPONSE_A   = "192.168.0.100";
 const char *MY_ADDRESS   = "192.168.33.1";
 const char *BIND_ADDRESS = "192.168.33.1";
+const char *CNAME        = "cname.example.com";
 
-class TC1Server : public dns::DNSServer
+class CNAMEServer : public dns::DNSServer
 {
 public:
-    TC1Server( const std::string addr, uint16_t port )
+    CNAMEServer( const std::string addr, uint16_t port )
 	: dns::DNSServer( addr, port )
     {}
 
@@ -19,25 +20,41 @@ public:
 	dns::ResponseInfo response;
 	dns::QuestionSectionEntry query_question = query.question[0];
 
+	std::string cname;
+	for ( int i = 0 ; i < 300 ; i++ ) {
+	    cname += "1.";
+	}
+	cname += "example.com";
+
 	dns::QuestionSectionEntry question;
 	question.q_domainname = query_question.q_domainname;
 	question.q_type       = query_question.q_type;
 	question.q_class      = query_question.q_class;
 	response.question_section.push_back( question );
 
-	dns::ResponseSectionEntry answer;
-	answer.r_domainname    = query_question.q_domainname;
-	answer.r_type          = dns::TYPE_A;
-	answer.r_class         = dns::CLASS_IN;
-	answer.r_ttl           = TTL;
-	answer.r_resource_data = dns::ResourceDataPtr( new dns::RecordA( RESPONSE_A ) );
-	response.answer_section.push_back( answer );
+	if ( query_question.q_domainname != cname ) {
+	    dns::ResponseSectionEntry answer1;
+	    answer1.r_domainname    = query_question.q_domainname;
+	    answer1.r_type          = dns::TYPE_CNAME;
+	    answer1.r_class         = dns::CLASS_IN;
+	    answer1.r_ttl           = TTL;
+	    answer1.r_resource_data = dns::ResourceDataPtr( new dns::RecordCNAME( cname ) );
+	    response.answer_section.push_back( answer1 );
+	}
+
+	dns::ResponseSectionEntry answer2;
+	answer2.r_domainname    = cname;
+	answer2.r_type          = dns::TYPE_A;
+	answer2.r_class         = dns::CLASS_IN;
+	answer2.r_ttl           = TTL;
+	answer2.r_resource_data = dns::ResourceDataPtr( new dns::RecordA( RESPONSE_A ) );
+	response.answer_section.push_back( answer2 );
   
 	response.header.id                   = htons( query.id );
 	response.header.opcode               = 0;
 	response.header.query_response       = 1;
 	response.header.authoritative_answer = 1;
-	response.header.truncation           = 1;
+	response.header.truncation           = 0;
 	response.header.recursion_desired    = 0;
 	response.header.recursion_available  = 0;
 	response.header.zero_field           = 0;
@@ -56,7 +73,7 @@ int main( int argc, char **argv )
 
     std::string bind_address;
 
-    po::options_description desc("TC=1 Server");
+    po::options_description desc("CNAME Server");
     desc.add_options()
         ("help,h",
          "print this message")
@@ -75,8 +92,9 @@ int main( int argc, char **argv )
         return 1;
     }
 
-    TC1Server server( bind_address, 53 );
+    CNAMEServer server( bind_address, 53 );
     server.start();
 
     return 0;
 }
+
